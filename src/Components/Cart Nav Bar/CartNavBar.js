c71c1e503d473d414b573ea2842931d853bf3c88
Ahmed Sayed Abdelwahab <ahm.abdelwahab@nu.edu.eg>
2022-04-25 04:49:02 +0300

    adding the server side [backend]

--- src/Components/Cart Nav Bar/CartNavBar.js
+++ src/Components/Cart Nav Bar/CartNavBar.js
@@ -10,12 +10,12 @@ class CartNavBar extends React.Component {
       <div id="shoppingCart">
         <img src={shoppingCart} alt="Shopping Cart" />
         {cartItems ? <p id="cartItemNum"> {cartItems} </p> : null}
-        <CartPage
+        {/* <CartPage
           miniCart={true}
           selectedProducts={this.props.selectedProducts}
           changeProdQuantity={this.props.changeProdQuantity}
           removeProd={this.props.removeProd}
-        />
+        /> */}
       </div>
     );
   }
